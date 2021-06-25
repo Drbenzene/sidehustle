a45a81b0f663388a42a6ba46e1d4c316bd76bacc
Drbenzene <enegxi1@gmail.com>
2021-06-24 20:20:08 -0500

    Adding week 1 task Solution

---
+++ week1.php
@@ -0,0 +1,26 @@
+<?php
+
+/*A range function that takes two arguments, start and end, 
+and returns an array containing all the numbers from start up to (and including) end.*/
+
+function myFunction(){
+
+    $result = array(range(1,50));
+
+    var_dump($result);
+
+}
+
+myFunction();
+
+// sum function that takes an array of numbers and returns the sum of these numbers.
+
+function sum(){
+    $numbers = array(2,6,10,15,45,60);
+    $addition = array_sum($numbers);
+    var_dump($addition);
+}
+
+sum();
+
+?>
